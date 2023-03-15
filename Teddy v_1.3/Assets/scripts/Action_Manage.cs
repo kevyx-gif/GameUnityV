f8c058da12e5ba7d13969ceff6e42b283d41f7e4
Kevin Garcia <apolortms@gmail.com>
2023-03-14 22:36:31 -0600

    Version 1.0

---
+++ Teddy v_1.3/Assets/scripts/Action_Manage.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Action_Manage : MonoBehaviour {
+	private Animator animator;
+	
+	public GameObject Target;
+	public int rutina;
+	public float cronometro;
+	public Quaternion angulo;
+	public float grado;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Comportamiento_Enemigo();
+	}
+
+	public void Comportamiento_Enemigo(){
+		if (Vector3.Distance(transform.position, Target.transform.position) > 5){
+			animator.SetBool("run",false);
+			cronometro += 1 * Time.deltaTime;
+
+			if (cronometro >= 4){
+				rutina = Random.Range(0,2);
+				cronometro = 0;
+			}
+
+			switch (rutina)
+			{
+				case 0:
+					animator.SetBool("walk",false);
+					break;
+				case 1:
+					grado = Random.Range(0,360);
+					angulo = Quaternion.Euler(0,grado,0);
+					rutina++;
+					break;
+				case 2:
+					transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+					transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+					animator.SetBool("walk",true);
+					break;
+
+			}
+		}
+		else if(Vector3.Distance(transform.position, Target.transform.position) > 1.6 && Vector3.Distance(transform.position, Target.transform.position) < 5){
+			var lookPos = Target.transform.position - transform.position;
+			lookPos.y = 0;
+			var rotation = Quaternion.LookRotation(lookPos);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+			animator.SetBool("atackMoment",false);
+			animator.SetBool("run",true);
+			transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+		}
+		else{
+			animator.SetBool("walk",false);
+			animator.SetBool("run",false);
+			animator.SetBool("atackMoment",true);
+		}
+	}
+}
---
+++ Teddy v_1.3/Assets/scripts/playerControl.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Animator animator;
+
+
+    public new Transform camera;
+    public float speed = 4;
+    public float gravity = -9.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Una forma
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 movement = Vector3.zero;
+        float movementspeed = 0;
+
+
+        if (hor != 0 || ver != 0){
+            Vector3 forward = camera.forward;
+            forward.y=0;
+            forward.Normalize();
+
+            Vector3 right = camera.right;
+            right.y = 0;
+            right.Normalize();
+
+            Vector3 direction = forward * ver + right * hor;
+            movementspeed = Mathf.Clamp01(direction.magnitude);
+            direction.Normalize();
+
+            movement = direction * speed * movementspeed *Time.deltaTime;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(direction),0.2f);
+        }
+
+        
+        movement.y += gravity + Time.deltaTime;
+        characterController.Move(movement);
+        animator.SetFloat("Speed", movementspeed );
+    }
+}
